83b4d43f168b675acadf4f274f7410851f7de162
Zak-White <D00232451@student.dkit.ie>
2023-11-04 12:41:47 +0000

    Finished Work on starting game states. Moving onto player

--- RayLibgames/vscode-template-main/GameState.cpp
+++ RayLibgames/vscode-template-main/GameState.cpp
@@ -1,29 +1,76 @@
 #include "GameState.hpp"
 
+double time;
 
+double GetCurrentTime() //the two following functions are used to help with the pause feature
+{
+    return time;
+}
+float GetDeltaTime()
+{
+    if(GameState== GameStates::Paused)
+    {
+        return 0;
+    }
+    else
+    {
+        return GetFrameTime();
+    }
+}
 
+bool updateGameState()
+{
+    if(GameState !=GameStates::Paused)
+    {
+        time += GetDeltaTime();
+    }
 
+    if(GameState == GameStates::Paused)
+    {
+        // Add code in here at later stage for functionality
+    }
+
+    if(GameState ==GameStates::Playing)
+    {
+        if(IsKeyPressed(KEY_ESCAPE))
+        {
+            GameState = GameStates::Paused;
+        }
+    }
+
+    if(GameState == GameStates::Won)
+    {
+        //Add functionality at a later stage
+    }
+
+    if(GameState == GameStates::Dead)
+    {
+        //Add functionality at a later stage 
+    }
+}
 void DrawWin()
 {
-    DrawText("Paused, escape to quit, click to resume",0,0,30,BLUE);
+    DrawText("Winning, escape to quit, click to resume",0,0,30,BLUE);
 }
 void DrawDead()
 {
-    DrawText("Paused, escape to quit, click to resume",0,0,30,BLUE);
+    DrawText("Dead, escape to quit, click to resume",0,0,30,BLUE);
 }
 void DrawGame()
 {
-    DrawText("Paused, escape to quit, click to resume",0,0,30,BLUE);
+    DrawText("Playing, escape to quit, click to resume",0,0,30,BLUE);
 }
 void DrawMenu()
 {
-    DrawText("Paused, escape to quit, click to resume",0,0,30,BLUE);
+    DrawText("Menu Times, escape to quit, click to resume",0,0,30,BLUE);
 }
 
 void DrawPaused()
 {
     DrawText("Paused, escape to quit, click to resume",0,0,30,BLUE);
 }
+
+
 void DrawOverlay()
 {
     switch (GameState)
