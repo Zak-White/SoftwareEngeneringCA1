6146910cfccbe0a4878c5e8e9e171fed3aac04a6
Zak-White <D00232451@student.dkit.ie>
2023-11-13 18:19:19 +0000

    Started working on generic entity class

---
+++ RayLibgames/vscode-template-main/Entity.hpp
@@ -0,0 +1,9 @@
+#include "raylib.h"
+
+class Entity
+{
+    public:
+    Vector2 position;
+    Texture2D Sprite;
+    int speed;
+};
--- RayLibgames/vscode-template-main/Player.hpp
+++ RayLibgames/vscode-template-main/Player.hpp
@@ -1,6 +1,7 @@
 #include "raylib.h"
+#include "Entity.hpp"
 
-class Player
+class Player: public Entity
 {
 //set up a default player at a later stage 
 public:
@@ -11,11 +12,16 @@ public:
     bool isMovingRight = false; //Setting up some example variables
     bool Alive = false; // set the players status in the game
 
+    Player()
 
     void move(); // Player movement function
+    void endGame();// end the game when the player hits a certain number of kills
     void direction(); //changes the players sprite based on the decoration travelled
     void checkBorder(); // stops the player going off the sides of the screen
     void unload(); // unloads the players textures
+
+    private:
+    int kills;
 };
 
 
